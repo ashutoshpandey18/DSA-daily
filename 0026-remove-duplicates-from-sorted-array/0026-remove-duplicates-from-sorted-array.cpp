32eddb6926d17e294f078330a6ae9cbc18fe976e
Ashutosh Pandey <147435071+ashutoshpandey18@users.noreply.github.com>
2026-09-04 21:11:15 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.7%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      int officer = 0;
-      int res = 1;
-      int cm = 1;
+        int officer = 0;
+        int res = 1;
+        int cm = 1;
 
-      while(cm<nums.size()){
-        if(nums[cm]==nums[cm-1]){
-            cm++;
-            continue;
+        int n = nums.size();
+
+        while(cm<n){
+            if(nums[cm]==nums[cm-1]){
+                cm++;
+              continue;
+            } else{
+              nums[officer+1] = nums[cm];
+              officer++;
+              res++;
+              cm++;
+            }
+
+        
+        
         }
-        else{
-            nums[officer+1]=nums[cm];
-            officer++;
-            res++;
-            cm++;
-        }
-      }
-      return res;
+
+        return res;
+        
     }
 };
